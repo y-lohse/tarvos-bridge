3687977c2fa252f495bc1115bee26051cf8b6d73
Yannick Lohse <glorf.travian@gmail.com>
2014-03-16 13:51:44 +0100

    Echo service & broadcasting scheduled messages

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ var WebSocketServer = require('ws').Server,
 	wss = new WebSocketServer({port: 8080});
 
 wss.on('connection', function(ws){
-	console.log('new connection!');
+	console.log('new connection');
 	
 	ws.on('message', function(message){
 		console.log('received %s', message);
 		ws.send(message);
 	});
+	
 	ws.send('welcome');
 });
+
+wss.broadcast = function(message){
+	console.log('broadcasting '+message);
+	for (var i in this.clients){
+		this.clients[i].send(message.toString());
+	}
+}
+
+var i = 0;
+setInterval(function(){
+	wss.broadcast(i++);
+}, 2000);
