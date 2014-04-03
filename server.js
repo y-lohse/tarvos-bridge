aa0737452b2886bd91e080e7d78ead7c2ae07543
Clowrid <clowrid@gmail.com>
2014-04-03 18:54:30 +0200

    Add type (in progress)

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function clientSetup(battle, client){
 	//creates the player inside the battle
 	API.getShip(client.token)
 	.then(function(data){
-		return battle.engine.pushTask(battle.engine.addPlayer, data.name, data.hp, data.armaments);
+		return battle.engine.pushTask(battle.engine.addPlayer, data.name, data.hp, data.armaments, data.type);
 	})
 	.then(function(player){
 		//assign the reference to the actual player object
